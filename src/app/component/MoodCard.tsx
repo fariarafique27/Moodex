8f377af57dc803c5433f250802b3717cfc14aa41
faria <faria.rafique27@gmail.com>
2025-07-10 14:35:45 +0500

    add return

--- src/app/component/MoodCard.tsx
+++ src/app/component/MoodCard.tsx
@@ -1,4 +1,4 @@
-export default function History(){
+export default function MoodCard(){
     return(
         <>
         </>
---
+++ src/app/dashboard/add-mode/page.tsx
@@ -0,0 +1,6 @@
+export default function AddMode(){
+    return(
+        <>
+        </>
+    )
+}
